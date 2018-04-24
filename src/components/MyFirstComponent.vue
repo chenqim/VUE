053ce0fa1737e587cb055a99ae7b82076f67f6b1
chenqim <chenqim@51bsi.com>
2018-04-24 10:41:03 +0800

    init vue project

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- 收藏弹窗 -->
+  <Modal v-model="collectModal">
+    <p slot="header">收藏</p>
+    <Form :model="collectForm" :label-width="80">
+      <FormItem label="名字：">
+        <Input v-model="collectForm.name" placeholder="请输入名称" />
+      </FormItem>
+      <FormItem label="分类：">
+        <Select v-model="collectForm.classify">
+          <Option value="0">默认</Option>
+          <Option value="1">喜欢的</Option>
+        </Select>
+      </FormItem>
+    </Form>
+  </Modal>
+</template>
+
+<script>
+export default {
+  name: 'MyFirstComponent',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      collectForm: {
+        name: '',
+        classify: ''
+      }
+    }
+  },
+  created () {
+  },
+  computed: {
+    collectModal: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/pages/MyFirst.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Button type="primary" size="large" @click="add">新增</Button>
+        <!-- 自定义组件v-model -->
+        <my-first-component v-model="collectShow"></my-first-component>
+    </div>
+</template>
+<script>
+import MyFirstComponent from '../components/MyFirstComponent'
+export default {
+  name: 'MyFirst',
+  components: {
+    MyFirstComponent
+  },
+  data () {
+    return {
+      collectShow: false
+    }
+  },
+  methods: {
+    add () {
+      this.collectShow = true
+      this.$Message.info('click add')
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
