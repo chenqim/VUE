80c11c13d8c9a7492ff4e5794904dadd0a9e3e1e
chenqim <chenqim@51bsi.com>
2018-08-21 17:39:44 +0800

    新增模版

---
+++ src/pages/Template.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+  name: 'Template',
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  created () {},
+  methods: {}
+}
+</script>
